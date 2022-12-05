ac8227df115e12337aa126c37e281d0dcc93c2a6
EmranSWE <mdemran.swe@gmail.com>
2022-12-05 21:35:36 +0600

    live server issue fixed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhuxjuh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://imran:uoNMXcL5UkblJfkP@cluster0.xhuxjuh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
